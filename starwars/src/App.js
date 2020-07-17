db9ef47cd94b806ba0cf47a3ded2ee91771698b1
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-07-17 11:15:54 -0400

    new branch, installed dependencies and axios, created container component for styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+
+const apiUrl = 'https://swapi.dev/api/'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,4 @@
 // Write your Character component here
+import React, { useState, useEffect} from 'react';
+
+export default Character;
