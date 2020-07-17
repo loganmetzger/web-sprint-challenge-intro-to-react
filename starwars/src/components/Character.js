7aa05dd4d93817d131d4dcb3af6fc869b872f049
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-07-17 12:34:22 -0400

    finally got everything mapped, no errors displaying

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,14 +1,24 @@
 // Write your Character component here
-import React, { useState, useEffect} from 'react';
+import React, { useState } from 'react';
 
 const Character = function(props) {
 
-    const { characters } = props;
+    const { character } = props;
 
-    console.log(characters)
+    console.log(character)
 
     return (
-       <h2>{props.name}</h2> 
+        <div>
+            <h2>{character.name}</h2> 
+            <p>Height: {character.height}</p>
+            <p>Mass: {character.mass}</p>
+            <p>Hair Color: {character.hair_color}</p>
+            <p>Skin Color: {character.skin_color}</p>
+            <p>Eye Color: {character.eye_color}</p>
+            <p>Birth Year: {character.birth_year}</p>
+            <p>Gender: {character.gender}</p>
+        </div>
+
     )
 
 }
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -5,14 +5,16 @@ import Character from './Character'
 const CharacterList = function(props) {
 
     const { characters } = props;
-    // console.log(characters)
+    console.log(characters)
+
+    // console.log(characters.map(character => {character}))
 
     return (
         <div>
             {
-                characters.map(char => {
-                    return (
-                        <Character characters={characters} />
+                characters.map((character) => {
+                    return(
+                        <Character character={character} />
                     )
                 })
             }
